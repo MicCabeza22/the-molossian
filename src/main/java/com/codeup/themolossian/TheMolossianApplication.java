8c3f8de341e3f70c04469783416b4544a0ce176e
MicCabeza22 <cabeza.michael@yahoo.com>
2018-03-01 11:48:19 -0600

    Initial push.

---
+++ src/main/java/com/codeup/themolossian/TheMolossianApplication.java
@@ -0,0 +1,12 @@
+package com.codeup.themolossian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheMolossianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheMolossianApplication.class, args);
+	}
+}
---
+++ src/test/java/com/codeup/themolossian/TheMolossianApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeup.themolossian;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TheMolossianApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
