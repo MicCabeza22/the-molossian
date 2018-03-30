183b434c768ac8c106dfe90058a0a9f4f0f0bead
MicCabeza22 <cabeza.michael@yahoo.com>
2018-03-30 14:15:53 -0500

    Added About page layout.

--- src/main/java/com/codeup/themolossian/controllers/HomeController.java
+++ src/main/java/com/codeup/themolossian/controllers/HomeController.java
@@ -9,4 +9,9 @@ public class HomeController {
     public String showHomePage() {
         return "home";
     }
+
+    @GetMapping("/about")
+    public String showAboutPage() {
+        return "about";
+    }
 }
