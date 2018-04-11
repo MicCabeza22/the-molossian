4c4ecd707a9f72350c5d50faa4992e727f10858e
MicCabeza22 <cabeza.michael@yahoo.com>
2018-04-11 14:39:32 -0500

    Committing changes...

--- src/main/java/com/codeup/themolossian/repositories/GameRepository.java
+++ src/main/java/com/codeup/themolossian/repositories/GameRepository.java
@@ -4,4 +4,5 @@ import com.codeup.themolossian.models.Game;
 import org.springframework.data.repository.CrudRepository;
 
 public interface GameRepository extends CrudRepository<Game, Long> {
+
 }
---
+++ src/main/java/com/codeup/themolossian/repositories/PlatformRepository.java
@@ -0,0 +1,8 @@
+package com.codeup.themolossian.repositories;
+
+import com.codeup.themolossian.models.Platform;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PlatformRepository extends CrudRepository<Platform, Long> {
+	
+}
