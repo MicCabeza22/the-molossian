038ef41b65add2ee423d39806896f55721c76072
MicCabeza22 <cabeza.michael@yahoo.com>
2018-05-25 13:27:29 -0500

    Added post delete functionality just in case.

--- src/main/java/com/codeup/themolossian/controllers/PostController.java
+++ src/main/java/com/codeup/themolossian/controllers/PostController.java
@@ -1,17 +1,21 @@
 package com.codeup.themolossian.controllers;
 
 import com.codeup.themolossian.models.Post;
+import com.codeup.themolossian.repositories.PostRepository;
 import com.codeup.themolossian.services.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PostController {
+	private final PostRepository postRepository;
     private PostService postService;
 
-    public PostController(PostService postService) {
+    public PostController(PostRepository postRepository, PostService postService) {
+    	this.postRepository = postRepository;
         this.postService = postService;
     }
 
@@ -31,6 +35,13 @@ public class PostController {
     	return "posts/create";
     }
     
+    @PostMapping("/posts/create")
+    public String createNewPost(Post post) {
+    	postRepository.save(post);
+    	
+    	return "redirect:/posts";
+    }
+    
     @GetMapping("/posts/{id}/edit")
     public String showEditPostForm(@PathVariable long id, Model model) {
     	Post post = postService.findOne(id);
@@ -38,5 +49,12 @@ public class PostController {
     	model.addAttribute("post", post);
     	
     	return "posts/edit";
+    }
+    
+    @PostMapping("/posts/{id}/delete")
+    public String deletePost(@PathVariable long id) {
+		postService.delete(id);
+    	
+    	return "redirect:/posts";
     }
 }
