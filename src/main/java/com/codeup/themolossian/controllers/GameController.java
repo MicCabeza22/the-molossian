078a06c10c3b1d866dd1fa05d17e508149eaf40d
MicCabeza22 <cabeza.michael@yahoo.com>
2018-05-30 16:02:08 -0500

    Adding genre selection to game addition form...

--- src/main/java/com/codeup/themolossian/controllers/GameController.java
+++ src/main/java/com/codeup/themolossian/controllers/GameController.java
@@ -2,12 +2,11 @@ package com.codeup.themolossian.controllers;
 
 import com.codeup.themolossian.models.EsrbRating;
 import com.codeup.themolossian.models.Game;
+import com.codeup.themolossian.models.Genre;
 import com.codeup.themolossian.repositories.EsrbRatingRepository;
 import com.codeup.themolossian.repositories.GameRepository;
+import com.codeup.themolossian.repositories.GenreRepository;
 import com.codeup.themolossian.services.GameService;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,12 +17,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class GameController {
 	private final EsrbRatingRepository esrbRatingRepository;
+	private final GenreRepository genreRepository;
 	private final GameRepository gameRepository;
     private GameService gameService;
 
-    public GameController(EsrbRatingRepository esrbRatingRepository, GameRepository gameRepository,
+    public GameController(EsrbRatingRepository esrbRatingRepository,
+    		GenreRepository genreRepository,
+    		GameRepository gameRepository,
     		GameService gameService) {
     	this.esrbRatingRepository = esrbRatingRepository;
+    	this.genreRepository = genreRepository;
         this.gameRepository = gameRepository;
     	this.gameService = gameService;
     }
@@ -40,8 +43,10 @@ public class GameController {
     @GetMapping("/games/add")
     public String showAddGameForm(Model model) {    	
        	Iterable<EsrbRating> esrbRatings = esrbRatingRepository.findAll();
+       	Iterable<Genre> genres = genreRepository.findAll();
     	
     	model.addAttribute("esrbRatings", esrbRatings);
+    	model.addAttribute("genre", genres);
     	model.addAttribute("game", new Game());
     	
     	return "games/add";
---
+++ src/main/java/com/codeup/themolossian/repositories/GenreRepository.java
@@ -0,0 +1,8 @@
+package com.codeup.themolossian.repositories;
+
+import com.codeup.themolossian.models.Genre;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GenreRepository extends CrudRepository<Genre, Long> {
+
+}
